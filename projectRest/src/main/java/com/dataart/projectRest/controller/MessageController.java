f4858f683ae032d0e22e827b9097c79b8aae795a
“Oleksandr_Pavlov” <oleksandr.pavlov@dataart.com>
2022-10-09 18:53:23 +0300

    list of messages

--- projectRest/src/main/java/com/dataart/projectRest/controller/MessageController.java
+++ projectRest/src/main/java/com/dataart/projectRest/controller/MessageController.java
@@ -1,14 +1,26 @@
 package com.dataart.projectRest.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
 @RequestMapping("message")
 public class MessageController {
+    public List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
+        add(new HashMap<String, String>(){{ put("id","1"); put("text","First message"); }});
+        add(new HashMap<String, String>(){{ put("id","2"); put("text","Second message"); }});
+        add(new HashMap<String, String>(){{ put("id","3"); put("text","Third message"); }});
+    }};
+
+
     @GetMapping
-    public String list() {
-        return "index";
+    public List<Map<String, String>> list() {
+        return messages;
     }
 }
