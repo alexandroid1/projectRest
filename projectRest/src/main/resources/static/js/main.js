b9f8bba256e8fc4197301255cf75e1d134f23abf
“Oleksandr_Pavlov” <oleksandr.pavlov@dataart.com>
2022-10-25 18:01:18 +0300

    vue js test messages list

--- projectRest/src/main/resources/static/js/main.js
+++ projectRest/src/main/resources/static/js/main.js
@@ -1,6 +1,17 @@
+// new component
+Vue.component('messages-list', {
+  props: ['messages'],
+  template: '<div><div v-for="message in messages">{{ message.text }}</div></div>'
+})
+
 var app = new Vue({
   el: '#app',
+  template: '<messages-list :messages="messages"/>',
   data: {
-    message: 'Hello, Vue!'
+    messages: [
+        { id: '123', text: 'first message'},
+        { id: '23', text: 'second message'},
+        { id: '3', text: 'third message'},
+    ]
   }
 });
