3cc7ad33b99d51eed4dfe947f9aab353316106fe
“Oleksandr_Pavlov” <oleksandr.pavlov@dataart.com>
2022-10-27 15:29:52 +0300

    Vue resource messages

--- projectRest/src/main/resources/static/js/main.js
+++ projectRest/src/main/resources/static/js/main.js
@@ -1,3 +1,5 @@
+var messageApi = Vue.resource('/message{/id}')
+
 Vue.component('message-row', {
     props: ['message'],
     template:  '<div><i>( {{ message.id }} )</i> {{ message.text }}</div>'
@@ -5,17 +7,22 @@ Vue.component('message-row', {
 
 Vue.component('messages-list', {
   props: ['messages'],
-  template: '<div><message-row v-for="message in messages" :message="message"/></div></div>'
+  template: '<div>' +
+    '<message-row v-for="message in messages" :key="message.id" :message="message"/></div>'+
+  '</div>',
+  created: function() {
+    messageApi.get().then( result =>
+     { result.json().then(data =>
+        data.forEach(message => this.messages.push(message))
+        )
+     })
+  }
 })
 
 var app = new Vue({
   el: '#app',
   template: '<messages-list :messages="messages"/>',
   data: {
-    messages: [
-        { id: '123', text: 'first message'},
-        { id: '23', text: 'second message'},
-        { id: '3', text: 'third message'},
-    ]
+    messages: []
   }
 });
